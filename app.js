1231c11d0b610a92298f02f59619283fa31d5087
Marka Rush <markabodine@gmail.com>
2017-12-06 19:01:19 -0600

    Checkpoint 8 DOM: completed

--- app.js
+++ app.js
@@ -1,55 +1,57 @@
 function onReady() {
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
-  const deleteChecked = document.getElementById('deleteChecked');
+   let toDos = [];
+   let id = 0;
+   const addToDoForm = document.getElementById('addToDoForm');
 
-  deleteChecked.addEventListener('click', () => {
-    toDoList = remove(newToDoText.id);
-    renderTheUI();
-  });
-// }
+   function createNewToDo() {
+     const newToDoText = document.getElementById('newToDoText');
+     if (!newToDoText.value) { return; }
 
-      //node.parentNode.removeChild(node);
-      //console.log(deleteChecked);
-      // Grab all checkboxes, loop over each one, and see if it is checked.
-      // If it is NOT checked, do nothing with it.
-      // If it IS checked, we need to remove its parent <li> from the <ul>.
-      // Hints:
-      //  .checked (boolean) property on checkbox elements
-      //  .parentElement (HTMLElement) property on all elements
-      //  Google how to remove child element (li) from a parent (ul) in JavaScript
-//  }, false);
+     toDos.push({
+       title: newToDoText.value,
+       complete: false,
+       id: id++
+     });
+
+     newToDoText.value = '';
+
+     renderTheUI();
+   }
+
+   function remove(id){
+     return toDos.filter(toDo => toDo.id !== id);
+   }
+
+   addToDoForm.addEventListener('submit', event => {
+     event.preventDefault();
+     createNewToDo();
+     newToDoText.value = '';
+   });
+
+   function renderTheUI() {
+     const toDoList = document.getElementById('toDoList');
+     toDoList.textContent = '';
+     toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+      const deleteBtn = document.createElement('button');
+      deleteBtn.innerHTML = '<span>Delete</span>';
+
+      newLi.textContent = toDo.title;
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.appendChild(deleteBtn);
+
+      deleteBtn.addEventListener('click', () => {
+        toDos = remove(toDo.id);
+        renderTheUI();
+      });
+     });
+  }
 
 
-
-  addToDoForm.addEventListener('submit', event => {
-    event.preventDefault();
-
-    //get the text
-    let title = newToDoText.value;
-
-    //create a new li
-    let newLi = document.createElement('li');
-
-    //create a new input
-    let checkbox = document.createElement('input');
-
-    //set the input's type to checkbox
-    checkbox.type = "checkbox";
-
-    //set the title
-    newLi.textContent = title;
-
-    //attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    //attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //empty the input
-    newToDoText.value = '';
-  });
+renderTheUI();
 }
 
 
